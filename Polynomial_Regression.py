511fbf0a2098399e84d5eba5ec275f73ad9fcb26
Doaa Khaled <36832526+DoaaKhalid@users.noreply.github.com>
2019-12-01 11:54:38 +0200

    Update Polynomial_Regression.py

--- Polynomial_Regression.py
+++ Polynomial_Regression.py
@@ -9,7 +9,7 @@ class Polynomial_Regression:
 
     def __init__(self, X_data, Y_data):
         Y_data = np.expand_dims(Y_data, axis=2)
-        poly_features = PolynomialFeatures(degree=1)
+        poly_features = PolynomialFeatures(degree=2)
         poly_X_data = poly_features.fit_transform(X_data.T)
         self.Xdata = poly_X_data
         self.Ydata = Y_data
